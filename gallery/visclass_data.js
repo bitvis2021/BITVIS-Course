297c2eb893aa56894d3747d4ef671d7213928572
weixin_46066026 <3220251465@bit.edu.cn>
2026-06-04 23:05:02 +0800

    refactor(gallery): remove hardcoded TEASERS & AJAX dependency, use script-based dynamic loading
    - Replace hardcoded TEASERS object in visclass_gallery_2026.html with dynamic
      loading via <script src="resources/teasers.js"> to avoid CORS issues under
      file:// protocol
    - Add resources/manifest.json as metadata index of all teaser images, and
      resources/teasers.js auto-generated from manifest.json exposing window.TEASERS
    - Update gallery/resources/ with corrected/reordered teaser image files matching
      group IDs extracted from course materials
    - Convert visclass.csv to visclass_data.js (window.VISCLASS_DATA) and load via
      <script> tag instead of $.ajax(), eliminating the only remaining XHR call
      - visclass.csv updated with group info data extracted from course materials
    - Remove parseCSV(), recordsFromCSV(), loadManifest(), loadAndRender() functions
    - Add recordsFromArray() to consume the new JS data format
    - Gallery now works on both file:// and http:// without any server requirement

---
+++ gallery/visclass_data.js
@@ -0,0 +1,218 @@
+window.VISCLASS_DATA = [
+  {
+    "id": 1,
+    "page": "https://zolaxiang-11.github.io/nyc_finalwork/",
+    "github": "https://github.com/Zolaxiang-11/nyc_finalwork",
+    "members": "赵婉彤 向左 朱写 郑舒涵"
+  },
+  {
+    "id": 2,
+    "page": "https://zcqa.github.io/final_vis/",
+    "github": "https://github.com/zcqa/final_vis",
+    "members": "朱策"
+  },
+  {
+    "id": 3,
+    "page": "https://synthase21.github.io/earthquake-vis/",
+    "github": "https://github.com/Synthase21/earthquake-vis",
+    "members": "钟志东 王舒晗 黄一杨"
+  },
+  {
+    "id": 4,
+    "page": "https://eric-wenyv.github.io/Cluster-Pulse-V3/#m=cpu&w=640%2C648&mi=544",
+    "github": "https://github.com/eric-wenyv/Cluster-Pulse-V3",
+    "members": "梁淏 王兆鲁 云若飞 刘浩翔"
+  },
+  {
+    "id": 5,
+    "page": "https://fibonacc12358.github.io/steam-pricing-psychology/",
+    "github": "github.com/fibonacc12358/steam-pricing-psychology/",
+    "members": "邹明臻 李嘉豪"
+  },
+  {
+    "id": 6,
+    "page": "https://fan-525.github.io/covid-policy-viz/",
+    "github": "https://github.com/fan-525/covid-policy-viz",
+    "members": "王奕凡 邢嘉钰 金姜恒 刘小贝"
+  },
+  {
+    "id": 7,
+    "page": "",
+    "github": "https://github.com/ZhlZhlZh/llmviz.github.io",
+    "members": "赵海林 孙济勋 赖宇琦 冯丽嘉"
+  },
+  {
+    "id": 8,
+    "page": "https://jaydhwid-boop.github.io/earthquake-viz/",
+    "github": "https://github.com/jaydhwid-boop/earthquake-viz",
+    "members": "周佳仪"
+  },
+  {
+    "id": 9,
+    "page": "",
+    "github": "https://xsy256.github.io/agricultural_vis/",
+    "members": "徐绍原 龙宇星"
+  },
+  {
+    "id": 10,
+    "page": "",
+    "github": "",
+    "members": "朱念成"
+  },
+  {
+    "id": 11,
+    "page": "",
+    "github": "https://github.com/Mapler-S/life-evolution-theatre.git",
+    "members": "郑远 董玳瑄"
+  },
+  {
+    "id": 12,
+    "page": "https://aoyama897.github.io/-1/",
+    "github": "https://aoyama897.github.io/-1/",
+    "members": "谢宝玛"
+  },
+  {
+    "id": 13,
+    "page": "",
+    "github": "",
+    "members": "张博 赵舒妍 刘欣媛"
+  },
+  {
+    "id": 14,
+    "page": "https://re1nyyy.github.io/Global-Carbon-Accountability-Tracker/",
+    "github": "https://github.com/Re1nyyy/Global-Carbon-Accountability-Tracker",
+    "members": "洪玺 张凯翔 陈炜达 王智奇"
+  },
+  {
+    "id": 15,
+    "page": "",
+    "github": "",
+    "members": "苑毅 涂睿轩 赵海宁"
+  },
+  {
+    "id": 16,
+    "page": "https://tshrer.github.io/datavis/",
+    "github": "",
+    "members": "杨若溪 刘栋 宫可鑫 刘志鹏"
+  },
+  {
+    "id": 17,
+    "page": "https://mrdftcg.github.io/Data-Visualization-Final-Work/",
+    "github": "https://github.com/Mrdftcg/Data-Visualization-Final-Work",
+    "members": "宁雨婷 刘宸宇"
+  },
+  {
+    "id": 18,
+    "page": "https://xiaonengzhixiong.github.io/olympic-power-shift/olympic_power_shift_3.1_with_country_comparsion.html",
+    "github": "https://github.com/xiaonengzhixiong/olympic-power-shift",
+    "members": "汪嘉玥 伍家宜 陈曦 王逸飞"
+  },
+  {
+    "id": 19,
+    "page": "https://guoyufei30.github.io/music-popularity/",
+    "github": "https://github.com/guoyufei30/music-popularity",
+    "members": "郭雨霏"
+  },
+  {
+    "id": 20,
+    "page": "https://11111why.github.io/earthquake-visualization/",
+    "github": "https://github.com/11111why/earthquake-visualization",
+    "members": "王明洋 汪涵宇 郭宇航"
+  },
+  {
+    "id": 21,
+    "page": "http://3uyuan1ee.me/ev/",
+    "github": "https://github.com/3uyuan1ee/ev",
+    "members": "李慕远 居雯惠 曹凯淇"
+  },
+  {
+    "id": 22,
+    "page": "https://reading-visualization-072add.pages.jihulab.net/",
+    "github": "https://github.com/blitheLL/reading-visualization",
+    "members": "刘佳妮 杜春雨"
+  },
+  {
+    "id": 23,
+    "page": "https://len3081.github.io/Big-Data-Visualization-Final-Project/",
+    "github": "https://github.com/len3081/Big-Data-Visualization-Final-Project",
+    "members": "邱昌威"
+  },
+  {
+    "id": 24,
+    "page": "",
+    "github": "",
+    "members": "钱臻濠 陈泽"
+  },
+  {
+    "id": 25,
+    "page": "https://laughter-cx.github.io/streaming-visualization/",
+    "github": "https://github.com/Laughter-cx/streaming-visualization",
+    "members": "崔晨曦 黄奕晨 胡贸曦 李雪丽"
+  },
+  {
+    "id": 26,
+    "page": "",
+    "github": "",
+    "members": "门金晓"
+  },
+  {
+    "id": 27,
+    "page": "",
+    "github": "",
+    "members": "陈逸羽"
+  },
+  {
+    "id": 28,
+    "page": "https://github.com/XIANAIGUAN/Visualization-of-Infant-Mortality-Rate",
+    "github": "https://github.com/XIANAIGUAN/Visualization-of-Infant-Mortality-Rate",
+    "members": "关显艾"
+  },
+  {
+    "id": 29,
+    "page": "https://okasaki514.github.io/movie-visualization/",
+    "github": "https://github.com/okasaki514/movie-visualization",
+    "members": "李思蓝 黄昊天 吴民瀚 休莎"
+  },
+  {
+    "id": 30,
+    "page": "https://colynchee.github.io/social-ecommerce-analysis/",
+    "github": "https://github.com/ColynChee/social-ecommerce-analysis",
+    "members": "吴伟彰 陈炳申 徐詠荃"
+  },
+  {
+    "id": 31,
+    "page": "https://giveyouhotpotbase.github.io/sjfx",
+    "github": "https://github.com/GiveYouHotPotBase/sjfx",
+    "members": "谢恩平"
+  },
+  {
+    "id": 32,
+    "page": "",
+    "github": "",
+    "members": "杜春喆"
+  },
+  {
+    "id": 33,
+    "page": "",
+    "github": "https://github.com/Indexohmany/Global-Sentiment-Flow-Mapper",
+    "members": "何宇 颖星 温迪 唐飞"
+  },
+  {
+    "id": 34,
+    "page": "https://andrew213123.github.io/made_in_china/",
+    "github": "https://github.com/Andrew213123/made_in_china",
+    "members": "张景飞 王佳乐"
+  },
+  {
+    "id": 35,
+    "page": "",
+    "github": "",
+    "members": "杨逸"
+  },
+  {
+    "id": 36,
+    "page": "",
+    "github": "",
+    "members": "张博涵"
+  },
+];
